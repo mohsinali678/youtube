7e4d7c37442b9b4dbc811e2d1f6fe38e37379ec3
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-05-24 10:41:27 -0400

    navbar created

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Link, Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import Home from './Components/Home'
+import About from './Components/About'
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+
+  }
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to='/'>Home</Link>
+          {" "}
+          <Link to='/about'>About</Link>
+        </nav>
+        <div>
+          <Route exact path='/' component={Home} /> 
+          <Route path='/about' component={About} />
+        </div>
+
+      </div>
+    )
+  }
 }
-
-export default App;
